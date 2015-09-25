694a7f03272912ec40b2d777c9f954371f4325f3
Alejandra Diaz <diaz.rivera.alejandra@gmail.com>
2015-09-24 21:23:02 -0400

    update

--- js/app.js
+++ js/app.js
@@ -1,27 +1,37 @@
 $(document).ready(function() {
+  var isactive=false;
   $(".ryu").mouseenter(function() {
-    $(".ryu-still").hide();
+  	if(!isactive) {
+  	$(".ryu-still").hide();
+    $(".ryu-throwing").hide();
     $(".ryu-ready").show();
+  	}
+
   })
 	.mouseleave(function() {
 	$(".ryu-ready").hide();
+	 $(".ryu-throwing").hide();
 	$(".ryu-still").show();
 	})
 	.mousedown(function() {
 	playHadouken();
 	$(".ryu-ready").hide();
+	$(".ryu-still").hide();
 	$(".ryu-throwing").show();
+	isactive=true;
 	$(".hadouken").finish().show().animate(
 		{"left":"1020px"}, 500,
 		function() {
 			$(this).hide();
 			$(this).css("left","520px");
+			isactive=false;
 	});
 })
 	.mouseup(function() {
 	$(".ryu-throwing").hide();
+	$(".ryu-still").hide();
 	$("ryu-ready").show();
-
+	
 	});
 });
 function playHadouken () {
